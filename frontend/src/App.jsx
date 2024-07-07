d7599e9f120d7d7f7378e380678f78b030f26d92
torsd <torsdalid@gmail.com>
2024-07-07 18:53:34 +0200

    (づ｡◕‿‿◕｡)づ

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,58 @@
 import React from "react";
+import { polygonAmoy } from "viem/chains"; 
+import { createPublicClient, getContract, parseAbi, http } from "viem";
 
-const LEADERBOARD = [
-  ["https://rpc.a.com", 0.2, 1000],
-  ["https://rpc.b.com", 0.3, 2000],
-  ["https://rpc.c.com", 0.5, 3000]
-]
+export const contractAbi = [
+  "function getUsers() view returns (address[] memory)",
+  "function userExists(address) view returns (bool)",
+  "function userScore(address) view returns (int256)",
+  "function users(uint256) view returns (address)",
+];
+export const contractAddress = "0xe666Bca53207d724D9f214dC792dfa2a873a10b6"
+
+export const rpc = createPublicClient({
+  chain: polygonAmoy,
+  batch: {
+    multicall: {
+      wait: 500,
+    },
+  },
+  transport: http(),
+});
 
 function App() {
-  const [leaderboard, setLeaderboard] = React.useState(LEADERBOARD)
+  const [leaderboard, setLeaderboard] = React.useState([])
 
   React.useEffect(() =>{
+    async function fetchContract() {
+      const contract = getContract({
+        address: contractAddress,
+        abi: parseAbi(contractAbi),
+        client: rpc,
+      })
+      const users = await contract.read.getUsers();
+      const scores = []
+      for (const u of users) {
+        scores.push(
+          await contract.read.userScore([u])
+        )
+      }
+      const leaderboard = []
+      for (let i = 0; i < users.length; i++) {
+        leaderboard.push([
+          users[i],
+          parseInt(scores[i].toString()),
+        ])
+      }
+      setLeaderboard(leaderboard.sort((a,b) => b[1]-a[1]));
+    }
+
+    fetchContract();
     let interval = setInterval(() => {
-      setLeaderboard(leaderboard);
+      // setLeaderboard(leaderboard);
     }, 1000)
     return () => clearInterval(interval)
-  })
+  }, [rpc])
 
   return (
     <div className="flex items-center justify-center h-screen flex-col mt-[-100px]">
@@ -27,27 +65,33 @@ function App() {
       </div>
       <div className="w-[800px] border">
         <table className="w-full">
-          <tr>
-            <th className="text-left pl-2 bg-gray-100/50 py-2">RPC</th>
-            <th className="text-left bg-gray-100/50 py-2">Latency</th>
-            <th className="text-left bg-gray-100/50 py-2">Reputation</th>
-            <th className="text-left bg-gray-100/50 py-2 text-center">Score</th>
-          </tr>
-          {
-            leaderboard.map(l => {
-              return (
-                <tr key={l[0]}>
-                  <td className="py-2 border-b border-black/10 pl-2">{l[0]}</td>
-                  <td className="py-2 border-b border-black/10">{l[1]}</td>
-                  <td className="py-2 border-b border-black/10 text-left">{l[2]}</td>
-                  <td className="py-2 border-b border-black/10 text-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="green" class="w-5 h-5 mx-auto"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path></svg>
-                  </td>
-                </tr>
-              )
-            })
-          }
+          <thead>
+            <tr>
+              <th className="text-left pl-2 bg-gray-100/50 py-2">RPC</th>
+              <th className="text-left bg-gray-100/50 py-2">Address</th>
+              <th className="text-left bg-gray-100/50 py-2 text-left">Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              leaderboard.map(l => {
+                return (
+                  <tr key={l[0]}>
+                    <td className="py-2 border-b border-black/10 pl-2">{l[0].slice(0,6).replace("0x","")}.rpc.node</td>
+                    <td className="py-2 border-b border-black/10 font-mono text-sm">{l[0]}</td>
+                    <td className="py-2 border-b border-black/10 text-left">{l[1]?.toString()}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
         </table>
+        {
+          leaderboard.length === 0 &&
+            <div className="bg-gray-100/20 p-4 flex items-center justify-center">
+              <span>Loading...</span>
+            </div>
+        }
       </div>
     </div>
   )
