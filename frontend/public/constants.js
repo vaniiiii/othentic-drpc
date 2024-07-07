a09915cecc3b383e1ebddc82ea3fe8b21e0e9b43
vaniiiii <ivandzambas@gmail.com>
2024-07-07 18:24:40 +0200

    Added constants file

---
+++ frontend/public/constants.js
@@ -0,0 +1,7 @@
+export const contractAbi = [
+  "function getUsers() view returns (address[] memory)",
+  "function userExists(address) view returns (bool)",
+  "function userScore(address) view returns (int256)",
+  "function users(uint256) view returns (address)",
+];
+export const contractAddress = "0xe666Bca53207d724D9f214dC792dfa2a873a10b6"
